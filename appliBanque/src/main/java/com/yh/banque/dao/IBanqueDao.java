73569c73fd00dd3de2de6d6b78344d0ad3c13ca4
youssef harkati <someone@nowhere.com>
2015-04-09 18:44:04 +0200

    implemented DAO interface

---
+++ appliBanque/src/main/java/com/yh/banque/dao/IBanqueDao.java
@@ -0,0 +1,27 @@
+package com.yh.banque.dao;
+
+import java.util.List;
+
+import com.yh.banque.entities.Client;
+import com.yh.banque.entities.Compte;
+import com.yh.banque.entities.Employe;
+import com.yh.banque.entities.Groupe;
+import com.yh.banque.entities.Operation;
+
+public interface IBanqueDao {
+  public Client addClient(Client c);
+  public Employe addEmploye(Employe e, Long codeSup);
+  public Groupe addGroupe(Groupe g);
+  public void addEmployeToGroupe(Long codeEmp, Long codeGroup);
+  public Compte addCompte(Compte cp, Long codeCli, Long codeEmp);
+  public Operation addOperation(Operation op, String codeCpte, Long codeEmp);
+  public Compte consulterCompte(String codeCpte);
+  public List<Operation> consulterOperations(String codeCpte);
+  public Client consulterClient(Long codeClient);
+  public List<Client> consulterClients(String mc);
+  public List<Compte> getComptesByClient(Long codeCli);
+  public List<Compte> getComptesByEmploye(Long codeEmp);
+  public List<Employe> getEmployes();
+  public List<Groupe> getGroupes();
+  public List<Employe> getEmployesByGroupe(Long codeGr);
+}
--- appliBanque/src/main/java/com/yh/banque/entities/Groupe.java
+++ appliBanque/src/main/java/com/yh/banque/entities/Groupe.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 @Entity
 public class Groupe implements Serializable{
    @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
    private Long codeGroupe;
    private String nomGroupe;
+   @ManyToMany(mappedBy="groupes")
    private Collection<Employe> employes;
    
    
